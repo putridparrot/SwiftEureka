b5453742dab7aea7165fa0ae6fff415eb08c356e
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2022-02-26 20:50:23 +0000

    Default init for DataCenterInfo

--- Sources/SwiftEureka/DataCenterInfo.swift
+++ Sources/SwiftEureka/DataCenterInfo.swift
@@ -2,6 +2,9 @@ public struct DataCenterInfo: Codable {
     var name: DataCenterName = DataCenterName.myown
     var cls: String = "com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo"
 
+    public init() {        
+    }
+
     public init(name: DataCenterName, cls: String) {
         self.name = name
         self.cls = cls
