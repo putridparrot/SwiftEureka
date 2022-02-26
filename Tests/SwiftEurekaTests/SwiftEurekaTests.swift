6c9c97579947b1cae61924c65efb3860b1572959
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2022-02-26 21:52:44 +0000

    Change to test file to get build working

---
+++ Tests/SwiftEurekaTests/SwiftEurekaTests.swift
@@ -0,0 +1,10 @@
+import XCTest
+import class Foundation.Bundle
+
+final class SwiftEurekaTests: XCTestCase {
+    // func testExample() throws {
+    //     guard #available(macOS 10.13, *) else {
+    //         return
+    //     }
+    // }
+}
